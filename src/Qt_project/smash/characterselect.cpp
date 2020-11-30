93968016f83a3fcd4fe80ca0f2ddf9512c1450a0
antti_tapsa <antti_tapsa@DESKTOP-832HVGP.localdomain>
2020-11-30 22:58:57 +0200

    changet the graphics of player 2 characters

--- src/Qt_project/smash/characterselect.cpp
+++ src/Qt_project/smash/characterselect.cpp
@@ -16,26 +16,40 @@ CharacterSelect::CharacterSelect(QGraphicsScene* scene, QStackedWidget *stack)
     }
     // add items into the scene
     QPushButton* totoro_p1_btn = new QPushButton();
-    totoro_p1_btn->setGeometry(QRect(150,400,300,100));
-    totoro_p1_btn->setText("Player 1 Totoro");
+
+    //icon for button
+    QIcon Icon1(characters_[2][0]);
+    totoro_p1_btn->setIcon(Icon1);
+    totoro_p1_btn->setIconSize(QSize(100,100));
+    totoro_p1_btn->move(150,400);
+    totoro_p1_btn->setMinimumSize(300,100);
     QObject::connect(totoro_p1_btn, SIGNAL(clicked()),this, SLOT(TotoroP1()));
     scene->addWidget(totoro_p1_btn);
 
     QPushButton* totoro_p2_btn = new QPushButton();
-    totoro_p2_btn->setGeometry(QRect(800,400,300,100));
-    totoro_p2_btn->setText("Player 2 Totoro");
+    QIcon Icon2(characters_[4][0]);
+    totoro_p2_btn->setIcon(Icon2);
+    totoro_p2_btn->setIconSize(QSize(100,100));
+    totoro_p2_btn->move(800,400);
+    totoro_p2_btn->setMinimumSize(300,100);
     QObject::connect(totoro_p2_btn, SIGNAL(clicked()),this, SLOT(TotoroP2()));
     scene->addWidget(totoro_p2_btn);
 
     QPushButton* tupsu_p1_btn = new QPushButton();
-    tupsu_p1_btn->setGeometry(QRect(150,300,300,100));
-    tupsu_p1_btn->setText("Player 1 Tupsu");
+    QIcon Icon3(characters_[1][0]);
+    tupsu_p1_btn->setIcon(Icon3);
+    tupsu_p1_btn->setIconSize(QSize(100,100));
+    tupsu_p1_btn->move(150,300);
+    tupsu_p1_btn->setMinimumSize(300,100);
     QObject::connect(tupsu_p1_btn, SIGNAL(clicked()),this, SLOT(TupsuP1()));
     scene->addWidget(tupsu_p1_btn);
 
     QPushButton* tupsu_p2_btn = new QPushButton();
-    tupsu_p2_btn->setGeometry(QRect(800,300,300,100));
-    tupsu_p2_btn->setText("Player 2 Tupsu");
+    QIcon Icon4(characters_[3][0]);
+    tupsu_p2_btn->setIcon(Icon4);
+    tupsu_p2_btn->setIconSize(QSize(100,100));
+    tupsu_p2_btn->move(800,300);
+    tupsu_p2_btn->setMinimumSize(300,100);
     QObject::connect(tupsu_p2_btn, SIGNAL(clicked()),this, SLOT(TupsuP2()));
     scene->addWidget(tupsu_p2_btn);
 
@@ -64,7 +78,7 @@ void CharacterSelect::TotoroP1(){
 }
 
 void CharacterSelect::TotoroP2(){
-    Player2_ = characters_[2];
+    Player2_ = characters_[4];
 }
 
 void CharacterSelect::TupsuP1(){
@@ -72,7 +86,7 @@ void CharacterSelect::TupsuP1(){
 }
 
 void CharacterSelect::TupsuP2(){
-    Player2_ = characters_[1];
+    Player2_ = characters_[3];
 }
 
 void CharacterSelect::ReturnToMain(){
--- src/Qt_project/smash/characterselect.h
+++ src/Qt_project/smash/characterselect.h
@@ -36,7 +36,9 @@ private:
     QStackedWidget* stack_;
     QGraphicsView* view_;
     std::map<int, QVector<QString>> characters_ = {{1, {":/images/tupsu.png", ":/images/tupsu_shove1.png", ":/images/tupsu_shove2.png", ":/images/tupsu_shove3.png", ":/images/tupsuleft.png"}},
-                                                   {2, {":/images/totoro.png", ":/images/totoro2.png", ":/images/totoro3.png", ":/images/totoro4.png", ":/images/totoro_left.png"}}};
+                                                   {2, {":/images/totoro.png", ":/images/totoro2.png", ":/images/totoro3.png", ":/images/totoro4.png", ":/images/totoro_left.png"}},
+                                                   {3, {":/images/tupsu_2.png", ":/images/tupsu_shove1_2.png", ":/images/tupsu_shove2_2.png", ":/images/tupsu_shove3_2.png", ":/images/tupsuleft_2.png"}},
+                                                   {4, {":/images/totoro_2.png", ":/images/totoro2_2.png", ":/images/totoro3_2.png", ":/images/totoro4_2.png", ":/images/totoro_left_2.png"}}};
     QVector<QString> Player1_;
     QVector<QString> Player2_;
     QTimer* timer_;
