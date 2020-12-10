317df16023847bfab611ee142b9bcfcc4cd5078e
antti_tapsa <antti_tapsa@DESKTOP-832HVGP.localdomain>
2020-12-10 15:50:50 +0200

    fixed smash.pro

---
+++ exe/Windows_executer/debug/moc_options.cpp
@@ -0,0 +1,155 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'options.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../src/Qt_project/smash/options.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'options.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Options_t {
+    QByteArrayData data[7];
+    char stringdata0[67];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Options_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Options_t qt_meta_stringdata_Options = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "Options"
+QT_MOC_LITERAL(1, 8, 8), // "QuitLoop"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 12), // "ReturnToMain"
+QT_MOC_LITERAL(4, 31, 11), // "SaveOptions"
+QT_MOC_LITERAL(5, 43, 11), // "SetKeybinds"
+QT_MOC_LITERAL(6, 55, 11) // "SetKeyValue"
+
+    },
+    "Options\0QuitLoop\0\0ReturnToMain\0"
+    "SaveOptions\0SetKeybinds\0SetKeyValue"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Options[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,   40,    2, 0x0a /* Public */,
+       4,    0,   41,    2, 0x0a /* Public */,
+       5,    0,   42,    2, 0x0a /* Public */,
+       6,    0,   43,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Options::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Options *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->QuitLoop(); break;
+        case 1: _t->ReturnToMain(); break;
+        case 2: _t->SaveOptions(); break;
+        case 3: _t->SetKeybinds(); break;
+        case 4: _t->SetKeyValue(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Options::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Options::QuitLoop)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Options::staticMetaObject = { {
+    QMetaObject::SuperData::link<QGraphicsView::staticMetaObject>(),
+    qt_meta_stringdata_Options.data,
+    qt_meta_data_Options,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Options::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Options::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Options.stringdata0))
+        return static_cast<void*>(this);
+    return QGraphicsView::qt_metacast(_clname);
+}
+
+int Options::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGraphicsView::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Options::QuitLoop()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
