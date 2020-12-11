535a8d3eef7e2195c634b72f90b1400e4e03e883
antti_tapsa <antti_tapsa@DESKTOP-832HVGP.localdomain>
2020-12-11 18:56:19 +0200

    changed the structure of src directory

--- src/Qt_project/build-smash-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainmenu.cpp
+++ src/Qt_project/build-smash-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainmenu.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../smash/mainmenu.h"
+#include "../../../smash/mainmenu.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- src/Qt_project/build-smash-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_options_view.cpp
+++ src/Qt_project/build-smash-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_options_view.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../smash/options_view.h"
+#include "../../../smash/options_view.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
